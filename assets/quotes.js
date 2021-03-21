2f07e9909d1ee6dcc445dfabacf2eb0f8be1209c
hidayatferdi135 <56286312+hidayatferdi135@users.noreply.github.com>
2021-03-21 11:45:42 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,10 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "avrill.v", quotes: "Assalamualaikum Ukhti" },
+    { author: "antjedtta_", quotes: "Udah Sholat Belum" },
+    { author: "standinshd", quotes: "Kalo sudah jangan lupa makan" },
+    { author: "standinshd", quotes: "Semangat" },
+    { author: "standinshd", quotes: "Jaga Kesehatan" },
+    { author: "standinshd", quotes: "Jangan lupa jaga hati" },
+    { author: "standinshd", quotes: "Aku masih berusaha jadi yang terbaik buatmu :)" },
 ];
